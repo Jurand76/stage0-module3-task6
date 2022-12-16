856020ec432ed9a84d2c64b4d51beb87fc1d1b46
Jurand76 <jurand@interia.pl>
2022-12-16 16:25:15 +0100

    fix: code fixed

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        float result;
+        result = Math.round(numberToBeRounded);
+        int result_final = (int)result;
+        System.out.println(result_final);
     }
 }
